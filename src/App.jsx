a12f4637a8bd573f310a6b75b45f014705f6db16
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-03-31 22:34:30 -0300

    solve infinite loop

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,56 @@ import Topbar from './components/Topbar';
 import Stories from './components/Stories';
 import Feed from './components/Feed';
 
+// Pensar nos estados -- ok
+// Pegar a lista de usuários --ok
+// Guardar lista de usuários no estado de APP --ok
+// Iterar sobre a lista de usuários pegandos os IDs e fazer requisições dos posts de cada um.
+// Guardar no estado a lista de posts;
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      posts: [],
+      usersFetched: 0,
+    }
+  }
+
+  componentDidMount() {
+    const usersList = fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/users');
+
+    usersList
+      .then((res) => res.json())
+      .then(dados => this.setState({ users:  dados }));
+  }
+
+  componentDidUpdate() {
+    const { users, posts, usersFetched } = this.state;
+
+    if (usersFetched === users.length) {
+      return;
+    }
+
+    fetch(`https://5e7d0266a917d70016684219.mockapi.io/api/v1/users/${users[usersFetched].id}/posts`)
+      .then((res) => res.json())
+      .then(dados => this.setState({
+        posts: [...posts, ...dados],
+        usersFetched: usersFetched + 1,
+      }))
+  }
+
   render() {
+    const { users, posts } = this.state;
+
+    console.log(posts);
+
     return (
       <React.Fragment>
         <Topbar />
 
-        <Stories />
-
-        <Feed />
+        <Feed posts={posts} />
       </React.Fragment>
     )
   }
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,10 +4,15 @@ import Post from './Post';
 
 class Feed extends React.Component {
   render() {
+    const { posts } = this.props;
+
     return (
       <div className="container">
         <section className="feed">
-          <Post />
+          { posts.length > 0
+            ? posts.map((cadaItem) => (<Post />))
+            : <h2> Não há nenhum post no feed <span role="img" aria-label="Emoji Triste">😭</span>!</h2>
+          }
         </section>
       </div>
     );
